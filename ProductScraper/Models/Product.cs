6b324956701f3d84e0a3a8c90591b622c751d912
Mihael Agopyan <mihael.b.agopyan@gmail.com>
2022-12-01 13:57:13 +0200

    Add project files.

---
+++ ProductScraper/Models/Product.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductScraper.Models
+{
+    internal class Product
+    {
+        private string _name;
+        private double _price;
+        private double _rating;
+
+        public Product(string name, double price, double rating)
+        {
+            _name = name;
+            _price = price;
+            _rating = rating;
+        }
+    }
+}
---
+++ ProductScraper/Program.cs
@@ -0,0 +1,56 @@
+﻿using HtmlAgilityPack;
+using System.Runtime.CompilerServices;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var path = @"../../../Excerpts/products.html";
+        var doc = new HtmlDocument();
+        try
+        {
+            doc.Load(path);
+        }
+        catch(DirectoryNotFoundException)
+        {
+            Console.WriteLine("Error: Directory " + path +" not found.");
+            return;
+        }
+
+        if (!CanParseHtmlDocument(doc.ParseErrors))
+            return;
+        else
+        {
+            Console.WriteLine("Success");
+            //if (htmlDoc.DocumentNode != null)
+            //{
+            //    HtmlAgilityPack.HtmlNode bodyNode = htmlDoc.DocumentNode.SelectSingleNode("//body");
+
+            //    if (bodyNode != null)
+            //    {
+            //        // Do something with bodyNode
+            //    }
+            //}
+        }
+    }
+
+    private static bool CanParseHtmlDocument(IEnumerable<HtmlParseError> errors)
+    {
+        if (errors != null && errors.Count() > 0)
+        {
+            ListParseErrors(errors);
+            return false;
+        }
+        else
+            return true;
+    }
+
+    private static void ListParseErrors(IEnumerable<HtmlParseError> errors)
+    {
+        Console.WriteLine("Error when parsing HTML file on lines:");
+        foreach (var htmlParseError in errors)
+        {
+            Console.WriteLine("- " + htmlParseError.Line);
+        }
+    }
+}
